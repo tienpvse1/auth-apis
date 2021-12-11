59b5141e5761ff5ce79d8cc253efd2820090eb76
tienpvse1 <tienpvse@fpt.edu.vn>
2021-12-11 14:03:58 +0700

    resolved(repository): add product repository

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,9 @@
-export class Product {}
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { BaseSchema } from 'src/base/base.schema';
+export type ProductDocument = Product & Document;
+
+@Schema({ collection: 'product' })
+export class Product extends BaseSchema {}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
